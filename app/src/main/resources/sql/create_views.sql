3fb106359d2bd99e06f6aeb6ab617e1da20bd06f
vladlukynov <lukynov.1@yandex.ru>
2022-11-28 12:27:25 +0300

    Update create_views.sql script

--- app/src/main/resources/sql/create_views.sql
+++ app/src/main/resources/sql/create_views.sql
@@ -1,10 +1,22 @@
 USE CarService;
 -- 1. Вывод списка сотрудников с количеством выполненных ими заказов за последний год, отсортированный по убыванию количества выполненных заказов
+GO
+CREATE VIEW GetEmployeesYearsOrders
+AS
+    SELECT UserLogin, FirstName, LastName, MiddleName, Post, Salary, Birthday, countTable.OrdersCount
+    FROM Employees
+            JOIN Orders ON Orders.EmployeeLogin = Employees.UserLogin,
+        (SELECT EmployeeLogin, COUNT(*) as OrdersCount FROM Orders GROUP BY EmployeeLogin) as countTable
+    WHERE DATEDIFF(dayofyear, Orders.StatusChangeDate , GETDATE()) <= 365 AND Status = N'Завершен'
+        AND countTable.EmployeeLogin = Orders.EmployeeLogin
+    ORDER BY countTable.OrdersCount;
+GO
+
 -- 2. Вывод информации о клиентах с их учетными данными
 GO
 CREATE VIEW GetClientsInfo
 AS
-    SELECT * FROM Clients
+    SELECT Users.UserLogin, FirstName, LastName, MiddleName, Birthday FROM Clients
         JOIN Users ON Clients.UserLogin = Users.UserLogin;
 GO
 
@@ -12,8 +24,8 @@ GO
 GO
 CREATE VIEW GetEmployeesInfo
 AS
-SELECT * FROM Employees
-    JOIN Users ON Employees.UserLogin = Users.UserLogin;
+    SELECT Users.UserLogin, FirstName, LastName, MiddleName, Post, Salary, Birthday FROM Employees
+        JOIN Users ON Employees.UserLogin = Users.UserLogin;
 GO
 
 -- 4. Вывод информации об автомобилях
@@ -39,18 +51,18 @@ GO
 
 -- 7. Вывод информации обо всех заказах с информацией об использованных деталях и оказанных услугах и стоимостью заказа
 GO
-CREATE VIEW GetOrderWithSum
+CREATE VIEW GetOrdersWithSum
 AS
-SELECT *, dbo.GetOrderSum(InfoTable.OrderId) as Total FROM
-    (SELECT Orders.OrderId, CarId, CarNumber, Status, CreationDate, StatusChangeDate, EmployeeLogin,
+    SELECT *, dbo.GetOrderSum(InfoTable.OrderId) as Total FROM
+        (SELECT Orders.OrderId, CarId, CarNumber, Status, CreationDate, StatusChangeDate, EmployeeLogin,
             ServiceName as Element, OrdersServices.Quantity, (OrdersServices.Quantity * Services.Price) as ElementSum
-    FROM Orders
-        JOIN OrdersServices ON Orders.OrderId = OrdersServices.OrderId
-        JOIN Services ON OrdersServices.ServiceId = Services.ServiceId
-    UNION
-    SELECT Orders.OrderId, CarId, CarNumber, Status, CreationDate, StatusChangeDate, EmployeeLogin,
-           DetailName, OrdersDetails.Quantity, (OrdersDetails.Quantity * Details.Price)
-    FROM Orders
-        JOIN OrdersDetails ON Orders.OrderId = OrdersDetails.OrderId
-        JOIN Details ON OrdersDetails.DetailId = Details.DetailId) as InfoTable;
+        FROM Orders
+            JOIN OrdersServices ON Orders.OrderId = OrdersServices.OrderId
+            JOIN Services ON OrdersServices.ServiceId = Services.ServiceId
+        UNION
+        SELECT Orders.OrderId, CarId, CarNumber, Status, CreationDate, StatusChangeDate, EmployeeLogin,
+            DetailName, OrdersDetails.Quantity, (OrdersDetails.Quantity * Details.Price)
+        FROM Orders
+            JOIN OrdersDetails ON Orders.OrderId = OrdersDetails.OrderId
+            JOIN Details ON OrdersDetails.DetailId = Details.DetailId) as InfoTable;
 GO
